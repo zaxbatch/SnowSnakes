b5478e2e9b73228d45ae5a930930f5f25e34d491
Zax <sneakjunkmail@gmail.com>
2026-08-19 17:43:25 -0400

    trying to deploy - htaccess again

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('✅ Snowsnakes server is running!');
+  res.send('✅ It works!');
 });
 
 app.listen(PORT, () => {
